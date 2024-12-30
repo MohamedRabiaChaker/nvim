886cd5b151aaee9522557421a6dd5788ce2c7fd4
Mohamed Rabii Chaker <chakerrabia9@gmail.com>
2024-12-30 11:36:02 +0100

    chadrc and lsp config changes

---
+++ lua/custom/lspconfig.lua
@@ -0,0 +1,26 @@
+local config = require "plugins.configs.lspconfig"
+
+local lspconfig = require "lspconfig"
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local function get_python_path_for_project()
+  local venv_path = vim.fn.findfile("bin/activate", vim.fn.getcwd() .. ";")
+
+  if venv_path ~= "" then
+    return vim.fn.fnamemodify(venv_path, ":h:h") .. "/bin/python"
+  end
+
+  return "/usr/bin/python"
+end
+
+lspconfig.pyright.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "python" },
+  python = {
+    settings = {
+      python = get_python_path_for_project(),
+    },
+  },
+}
