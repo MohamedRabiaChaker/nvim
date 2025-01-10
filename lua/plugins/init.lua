c2991e9e75c691c681c93ebc2ad68a74d11a6fb4
mohamed.chaker <mohamed.chaker@ataccama.com>
2025-01-10 15:13:51 +0100

    diffview installation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -341,6 +341,29 @@ local default_plugins = {
     end,
   },
   {
+    "sindrets/diffview.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      { "TimUntersberger/neogit", config = { disable_commit_confirmation = true } },
+    },
+    commit = "9359f7b1dd3cb9fb1e020f57a91f8547be3558c6", -- HEAD requires git 2.31
+    keys = {
+      { "<C-g>", "<CMD>DiffviewOpen<CR>", mode = { "n", "i", "v" } },
+    },
+    config = {
+      keymaps = {
+        view = {
+          ["<C-g>"] = "<CMD>DiffviewClose<CR>",
+          ["c"] = "<CMD>DiffviewClose|Neogit commit<CR>",
+        },
+        file_panel = {
+          ["<C-g>"] = "<CMD>DiffviewClose<CR>",
+          ["c"] = "<CMD>DiffviewClose|Neogit commit<CR>",
+        },
+      },
+    },
+  },
+  {
     "folke/which-key.nvim",
     keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
     init = function()
