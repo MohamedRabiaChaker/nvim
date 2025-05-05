8869fcebefc36b7864e935888231492b84a2c065
mohamed.chaker <mohamed.chaker@ataccama.com>
2025-05-05 15:26:06 +0200

    add dadbod dbui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -375,6 +375,23 @@ local default_plugins = {
       require("which-key").setup(opts)
     end,
   },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
 
 local config = require("core.utils").load_config()
